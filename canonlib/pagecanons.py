53a431618cf6ed8738e6a33c07ef1a97eb0474fe
Brandon Rhodes <brandon@rhodesmill.org>
2012-10-02 20:18:59 -0400

    Factor out drawing simply the page diagonals

--- canonlib/pagecanons.py
+++ canonlib/pagecanons.py
@@ -1,26 +1,33 @@
 
+def diagonals(canvas, is_right=False):
+    """Draw a diagonal across the page, and diagonals across the whole book."""
+
+    canvas.saveState()
+    w, h = canvas._pagesize
+    if is_right:
+        canvas.transform(-1., 0., 0., 1., w, 0.)
+
+    canvas.line(0, 0, w, h)             # main diagonal
+    canvas.line(0, 0, w, h / 2.)        # bottom half of cross-page diagonal
+    canvas.line(w, h / 2., 0, h)        # top half of cross-page diagonal
+
+    canvas.restoreState()
+
 def van_de_graaf_rectangle(canvas):
     w, h = canvas._pagesize
     return w * 2./9., h * 2./9., w * 6./9., h * 6./9.
 
-def van_de_graaf(canvas, is_right):
+def van_de_graaf(canvas, is_right=False):
     """Draw the Van de Graaf canon construction on the ReportLab `canvas`."""
 
-    canvas.saveState()
-    canvas.setLineWidth(0.5)
-    line = canvas.line
     w, h = canvas._pagesize
 
+    diagonals(canvas, is_right)
+
+    line = canvas.line
+    canvas.saveState()
     if is_right:
         canvas.transform(-1., 0., 0., 1., w, 0.)
-
-    # First we draw a diagonal from the lower outer corner to the top
-    # inner corner, then a pair of diagonals across the whole opened
-    # book, from each corner to its opposite.
-
-    line(0, 0, w, h)             # main diagonal
-    line(0, 0, w, h / 2.)        # bottom half of cross-page diagonal
-    line(w, h / 2., 0, h)        # top half of cross-page diagonal
 
     # Next, we figure out where the diagonals intersect.
 
