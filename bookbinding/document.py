eae76160b6caa952c51d7b45259b1dc5232dc5b8
Brandon Rhodes <brandon@rhodesmill.org>
2012-10-04 23:40:36 -0400

    The demand for lines can now create new pages

--- bookbinding/document.py
+++ bookbinding/document.py
@@ -1,4 +1,4 @@
-from reportlab.pdfgen import canvas
+from reportlab.pdfgen.canvas import Canvas
 
 inch = 72.
 
@@ -21,49 +21,83 @@ class Page(object):
     def __init__(self, document, ci):
         self.document = document
         self.ci = self.cj = ci
-        self.canvas = canvas.Canvas('book.pdf',
-                                    pagesize=(PAGE_WIDTH, PAGE_HEIGHT))
+        self.previous = None
+
+    def next(self):
+        p = Page(self.document, self.ci)
+        p.previous = self
+        return p
 
 class Chase(object):
-    def __init__(self, x, y, w, h):
+    def __init__(self, page, x, y, w, h):
+        self.page = page
         self.x = x
         self.y = y
         self.w = w
         self.h = h
+
+    def next(self):
+        return Chase(self.page.next(), self.x, self.y, self.w, self.h)
 
 class Document(object):
 
     def format(self, story):
 
         p = Page(self, 0)
-        c = Chase(OUTER_MARGIN, BOTTOM_MARGIN,
+        c = Chase(p, OUTER_MARGIN, BOTTOM_MARGIN,
                   PAGE_WIDTH - OUTER_MARGIN - INNER_MARGIN,
                   PAGE_HEIGHT - TOP_MARGIN - BOTTOM_MARGIN)
         p.cj += 1
-
-        p.canvas.setFont('Roman', FONT_SIZE)
 
         line = Line(p, c)
         for item in story:
             if not item.__class__.__name__.endswith('Paragraph'):
                 continue
             for s in [item.text]:
-                p.canvas.drawString(c.x, line.ay(), s)
+                line.text = s
                 line = line.next()
-        p.canvas.save()
+                line.text = 'THE END'
+
+        lines = []
+        while line:
+            lines.append(line)
+            line = line.previous
+
+        lines.reverse()
+
+        canvas = Canvas(
+            'book.pdf', pagesize=(PAGE_WIDTH, PAGE_HEIGHT))
+
+        canvas.setFont('Roman', FONT_SIZE)
+
+        page = None
+        for line in lines:
+            if line.page is not page:
+                canvas.showPage()
+                page = line.page
+            canvas.drawString(c.x, line.ay(), line.text)
+
+        canvas.save()
 
 class Line(object):
 
     def __init__(self, page, chase, previous=None, dy=0):
         self.page = page
         self.chase = chase
+        self.previous = previous
         if previous is None:
             self.y = chase.h - LINE_HEIGHT - dy
         else:
             self.y = previous.y - dy
 
     def next(self):
-        return Line(self.page, self.chase, self, LINE_HEIGHT)
+        if self.y > LINE_HEIGHT:
+            return Line(self.page, self.chase, self, LINE_HEIGHT)
+        else:
+            chase = self.chase.next()
+            line = Line(chase.page, chase, self)
+            line.y = chase.h - LINE_HEIGHT
+            return line
 
     def ay(self):
         return self.chase.y + self.y
