09b4e6b462d6176b1f5a3d63ce43e7d5d60f30c4
Brandon Rhodes <brandon@rhodesmill.org>
2012-10-04 21:51:54 -0400

    Render a list of paragraphs at a spot on one page

--- bookbinding/__init__.py
+++ bookbinding/__init__.py
@@ -1,1 +1,2 @@
 
+from .document import Document, Chase
---
+++ bookbinding/document.py
@@ -0,0 +1,52 @@
+from reportlab.pdfgen import canvas
+
+inch = 72.
+
+PAGE_WIDTH = 6. * 72.
+PAGE_HEIGHT = 9. * 72.
+
+INNER_MARGIN = 54.
+OUTER_MARGIN = inch
+BOTTOM_MARGIN = inch + 6.
+TOP_MARGIN = inch - 6.
+
+class Setter(object):
+    pass
+
+class Page(object):
+
+    def __init__(self, document, ci):
+        self.document = document
+        self.ci = self.cj = ci
+        self.canvas = canvas.Canvas('book.pdf',
+                                    pagesize=(PAGE_WIDTH, PAGE_HEIGHT))
+
+class Chase(object):
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+class Document(object):
+
+    def format(self, story):
+
+        pages = []
+        chases = []
+
+        p = Page(self, 0)
+        pages.append(p)
+        c = Chase(OUTER_MARGIN, BOTTOM_MARGIN,
+                  PAGE_WIDTH - OUTER_MARGIN - INNER_MARGIN,
+                  PAGE_HEIGHT - TOP_MARGIN - BOTTOM_MARGIN)
+        chases.append(c)
+        p.cj += 1
+
+        for item in story:
+            if not item.__class__.__name__.endswith('Paragraph'):
+                continue
+            lines = [item.text]
+            for line in lines:
+                p.canvas.drawString(c.x, c.y, line)
+        p.canvas.save()
