b45cc80501d97aa7749c42278fa526b8a0f9ab5a
Brandon Rhodes <brandon@rhodesmill.org>
2012-10-28 16:41:57 -0400

    Moved Knuth algorithm itself into "knuth" module

--- bookbinding/document.py
+++ bookbinding/document.py
@@ -1,10 +1,6 @@
 from reportlab.pdfgen.canvas import Canvas
-from .hyphenate import hyphenate_word
-from .knuth import KnuthLine
+from .knuth import wrap_paragraph
 from .skeleton import Page, Chase, Line
-from texlib.wrap import ObjectList, Box, Glue, Penalty
-
-import re
 
 inch = 72.
 
@@ -49,7 +45,11 @@ class Document(object):
                 #     line.align = 'center'
             elif isinstance(item, Paragraph):
                 for s in [item.text]:
-                    line = wrap_paragraph_knuth(canvas, line, item)
+                    if item.style == 'indented-paragraph':
+                        indent = FONT_SIZE
+                    else:
+                        indent = 0.0
+                    line = wrap_paragraph(canvas, line, item, indent)
 
         pages = line.unroll_document()
 
@@ -67,77 +67,6 @@ class Document(object):
                 graphic.draw(canvas)
 
         canvas.save()
-
-wordre = re.compile('(\w+)')
-STRING_WIDTHS = {}
-
-def wrap_paragraph_knuth(canvas, line, pp):
-
-    olist = ObjectList()
-    # olist.debug = True
-
-    if pp.style == 'indented-paragraph':
-        olist.append(Glue(FONT_SIZE, 0, 0))
-
-    space_width = canvas.stringWidth(u' ')
-    hyphen_width = canvas.stringWidth(u'-')
-
-    for word in pp.text.split():
-        pieces = hyphenate_word(word)
-        for piece in pieces:
-            w = STRING_WIDTHS.get(piece)
-            if w is None:
-                w = STRING_WIDTHS[piece] = canvas.stringWidth(piece)
-            olist.append(Box(w, piece))
-            olist.append(Penalty(hyphen_width, 100))
-        olist.pop()
-        olist.append(Glue(space_width, space_width * .5, space_width * .25))
-    olist.pop()
-    olist.add_closing_penalty()
-
-    # for item in olist:
-    #     print item
-
-    line_lengths = [line.w]
-    for tolerance in 1, 2, 3, 4:
-        try:
-            breaks = olist.compute_breakpoints(
-                line_lengths, tolerance=tolerance)
-        except RuntimeError:
-            pass
-        else:
-            break
-
-    start = 0
-    n = 0
-    # if 'Confederate' in pp.text:
-    #     print breaks, len(olist)
-    # if breaks[-1] != len(olist) - 1:
-    #     breaks.append(len(olist) - 1)
-    for breakpoint in breaks[1:]:
-        keepers = []
-        r = olist.compute_adjustment_ratio(start, breakpoint, 0, (line.w,))
-        # if 'Confederate' in pp.text:
-        #     print breakpoint, r
-        n += 1
-        for i in range(start, breakpoint):
-            box = olist[i]
-            if box.is_glue():
-                box.glue_width = box.compute_width(r)
-                keepers.append(box)
-            elif box.is_box():
-                keepers.append(box)
-        bbox = olist[breakpoint]
-        if bbox.is_penalty() and bbox.width == hyphen_width:
-            b = Box(hyphen_width, u'-')
-            keepers.append(b)
-        line = line.next()
-        graphic = KnuthLine(line)
-        graphic.things = keepers
-        line.graphics.append(graphic)
-        start = breakpoint + 1
-
-    return line
 
 class Paragraph(object):
 
--- bookbinding/knuth.py
+++ bookbinding/knuth.py
@@ -1,6 +1,77 @@
 """Classes that support Knuth TeX-style paragraph breaking."""
 
-from texlib.wrap import Box, Glue
+from texlib.wrap import ObjectList, Box, Glue, Penalty
+from .hyphenate import hyphenate_word
+
+STRING_WIDTHS = {}
+
+def wrap_paragraph(canvas, line, pp, indent):
+
+    olist = ObjectList()
+    # olist.debug = True
+
+    if indent:
+        olist.append(Glue(indent, 0, 0))
+
+    space_width = canvas.stringWidth(u' ')
+    hyphen_width = canvas.stringWidth(u'-')
+
+    for word in pp.text.split():
+        pieces = hyphenate_word(word)
+        for piece in pieces:
+            w = STRING_WIDTHS.get(piece)
+            if w is None:
+                w = STRING_WIDTHS[piece] = canvas.stringWidth(piece)
+            olist.append(Box(w, piece))
+            olist.append(Penalty(hyphen_width, 100))
+        olist.pop()
+        olist.append(Glue(space_width, space_width * .5, space_width * .25))
+    olist.pop()
+    olist.add_closing_penalty()
+
+    # for item in olist:
+    #     print item
+
+    line_lengths = [line.w]
+    for tolerance in 1, 2, 3, 4:
+        try:
+            breaks = olist.compute_breakpoints(
+                line_lengths, tolerance=tolerance)
+        except RuntimeError:
+            pass
+        else:
+            break
+
+    start = 0
+    n = 0
+    # if 'Confederate' in pp.text:
+    #     print breaks, len(olist)
+    # if breaks[-1] != len(olist) - 1:
+    #     breaks.append(len(olist) - 1)
+    for breakpoint in breaks[1:]:
+        keepers = []
+        r = olist.compute_adjustment_ratio(start, breakpoint, 0, (line.w,))
+        # if 'Confederate' in pp.text:
+        #     print breakpoint, r
+        n += 1
+        for i in range(start, breakpoint):
+            box = olist[i]
+            if box.is_glue():
+                box.glue_width = box.compute_width(r)
+                keepers.append(box)
+            elif box.is_box():
+                keepers.append(box)
+        bbox = olist[breakpoint]
+        if bbox.is_penalty() and bbox.width == hyphen_width:
+            b = Box(hyphen_width, u'-')
+            keepers.append(b)
+        line = line.next()
+        graphic = KnuthLine(line)
+        graphic.things = keepers
+        line.graphics.append(graphic)
+        start = breakpoint + 1
+
+    return line
 
 class KnuthLine(object):
     """A graphic that knows how to draw a justified line of text."""
