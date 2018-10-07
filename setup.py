c74b5afe0b85e7d4c8bc9057cf087d8f6e2318d9
Brandon Rhodes <brandon@rhodesmill.org>
2018-10-07 13:11:44 -0400

    Remove texlib from setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(name = 'bookbinding',
       classifiers = [
         ],
       packages = ['bookbinding'],
-      install_requires = ['reportlab', 'texlib'],
+      install_requires = ['reportlab'],
       )
