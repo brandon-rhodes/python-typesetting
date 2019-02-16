e44746cd00e63cbd6a0e6581e43cde1b79da414e
Brandon Rhodes <brandon@rhodesmill.org>
2019-02-16 18:54:35 -0500

    Add support for crop marks

--- bookbinding/document.py
+++ bookbinding/document.py
@@ -7,9 +7,10 @@ mm = 25.4 / 72
 
 class Document(object):
 
-    def __init__(self, page_width, page_height):
+    def __init__(self, page_width, page_height, margin_width=0):
         self.page_width = page_width
         self.page_height = page_height
+        self.margin_width = margin_width
 
         QApplication(['my-q-application'])
         f = QFontDatabase.addApplicationFont('OldStandard-Regular.ttf')
@@ -18,9 +19,52 @@ class Document(object):
         names = QFontDatabase.applicationFontFamilies(f)
         print(names)
         self.writer = QPdfWriter('book.pdf')
-        self.writer.setPageSizeMM(QSizeF(page_width * mm, page_height * mm))
-        self.writer.setPageMargins(QMarginsF(0, 0, 0, 0))
+        size = QSizeF((2 * margin_width + page_width) * mm,
+                      (2 * margin_width + page_height) * mm)
+        self.writer.setPageSizeMM(size)
+        margins = QMarginsF(margin_width, margin_width,
+                            margin_width, margin_width)
+        self.writer.setPageMargins(margins)
         self.painter = QPainter(self.writer)
+        self.draw_crop_marks()
+
+    def new_page(self):
+        self.writer.newPage()
+        self.draw_crop_marks()
+
+    def draw_crop_marks(self):
+        pt = 1200 / 72.0
+        m = self.margin_width * pt
+        offset = 9 * pt
+        weight = 0.25
+        painter = self.painter
+
+        if m < 9:
+            return
+        pen = QPen(Qt.black, weight, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin)
+        painter.setPen(pen)
+
+        w = self.page_width * pt
+        h = self.page_height * pt
+
+        for x in 0, w:
+            painter.drawLine(QPoint(x, -m), QPoint(x, -offset))
+            painter.drawLine(QPoint(x, h + m), QPoint(x, h + offset))
+        for y in 0, h:
+            painter.drawLine(QPoint(-m, y), QPoint(-offset, y))
+            painter.drawLine(QPoint(w + m, y), QPoint(w + offset, y))
+
+        # x, y = chase.x * pt, chase.y * pt
+        # w = chase.width * pt
+        # h = chase.height * pt
+
+        # painter.drawPolyline([
+        #     QPoint(x, y),
+        #     QPoint(x + w, y),
+        #     QPoint(x + w, y + h),
+        #     QPoint(x, y + h),
+        #     QPoint(x, y),
+        # ])
 
 def mark_chase(painter, chase):
     pt = 1200 / 72.0
